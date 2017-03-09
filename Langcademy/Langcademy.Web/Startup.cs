3d151bdc523e636223d7abcba6577a67aa0e68de
YavorYanchev <yavor_yanchev_bs@abv.bg>
2017-03-09 12:59:20 +0200

    Added web project

---
+++ Langcademy/Langcademy.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Langcademy.Web.Startup))]
+namespace Langcademy.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

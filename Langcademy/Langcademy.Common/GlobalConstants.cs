dd94aad469fc3bb70bcd0b9923223a84f3779644
YavorYanchev <yavor_yanchev_bs@abv.bg>
2017-03-09 12:49:47 +0200

    Skeleton creation

---
+++ Langcademy/Langcademy.Common/GlobalConstants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Langcademy.Common
+{
+    public class GlobalConstants
+    {
+    }
+}

83d2ab2993de16843950a9e66e4a98731d7b2123
YavorYanchev <yavor_yanchev_bs@abv.bg>
2017-03-10 21:36:48 +0200

    Added topicsService

---
+++ Langcademy/Services/Langcademy.Services.Data/TopicsService.cs
@@ -0,0 +1,35 @@
+﻿using Langcademy.Services.Data.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Langcademy.Data.Models;
+using Langcademy.Services.Web;
+using Langcademy.Data.Common;
+
+namespace Langcademy.Services.Data
+{
+    public class TopicsService : ITopicsService
+    {
+        private readonly IIdentifierProvider identifierProvider;
+        private readonly IDbRepository<Topic> topics;
+
+        public TopicsService(IDbRepository<Topic> topics, IIdentifierProvider identifierProvider)
+        {
+            this.topics = topics;
+            this.identifierProvider = identifierProvider;
+        }
+
+        public void Add(Topic topic)
+        {
+            if (topic == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            this.topics.Add(topic);
+            this.topics.Save();
+        }
+    }
+}

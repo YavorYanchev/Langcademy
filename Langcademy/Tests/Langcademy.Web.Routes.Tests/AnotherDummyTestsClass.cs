d47ce0b72a6f96d3f5f363f74fe0de4c89de52b2
YavorYanchev <yavor_yanchev_bs@abv.bg>
2017-03-13 14:54:51 +0200

    Added dummy tests for the route to test the job

---
+++ Langcademy/Tests/Langcademy.Web.Routes.Tests/AnotherDummyTestsClass.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Langcademy.Web.Routes.Tests
+{
+    [TestFixture]
+    public class AnotherDummyTestsClass
+    {
+        [Test]
+        public void DummyRouteTest1()
+        {
+            Assert.AreNotEqual(1, 2);
+        }
+
+    }
+}

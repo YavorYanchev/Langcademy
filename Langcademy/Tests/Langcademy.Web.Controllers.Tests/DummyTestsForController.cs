51f8d85ff62e34ed2d4cd526a0cc4a442ce7628a
YavorYanchev <yavor_yanchev_bs@abv.bg>
2017-03-13 14:08:14 +0200

    Added dummy test for the controller to test the job

---
+++ Langcademy/Tests/Langcademy.Web.Controllers.Tests/DummyTestsForController.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Langcademy.Web.Controllers.Tests
+{
+    [TestFixture]
+    public class DummyTestsForController
+    {
+        [Test]
+        public void FirstDummyTest()
+        {
+            Assert.AreEqual(10, 30 - 20);
+        }
+
+        [Test]
+        public void SecondDummyTest()
+        {
+            Assert.AreEqual(20, 30 - 10);
+        }
+    }
+}

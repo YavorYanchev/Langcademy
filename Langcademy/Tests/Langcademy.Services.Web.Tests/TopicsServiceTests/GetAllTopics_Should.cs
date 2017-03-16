f7b17fa5c944430a18721a112651ea1474a9673e
YavorYanchev <yavor_yanchev_bs@abv.bg>
2017-03-16 18:18:35 +0200

    Added getAllTopics and tested

---
+++ Langcademy/Tests/Langcademy.Services.Web.Tests/TopicsServiceTests/GetAllTopics_Should.cs
@@ -0,0 +1,33 @@
+﻿using Langcademy.Data.Common;
+using Langcademy.Data.Models;
+using Langcademy.Services.Data;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Langcademy.Services.Web.Tests.TopicsServiceTests
+{
+    [TestFixture]
+    public class GetAllTopics_Should
+    {
+        [Test]
+        public void ReturnIQuerableOfTopic()
+        {
+            // Arrange
+            var mockedRepository = new Mock<IDbRepository<Topic>>();
+            var mockedIdentifier = new Mock<IIdentifierProvider>();
+            var topicService = new TopicsService(mockedRepository.Object, mockedIdentifier.Object);
+            var mockedTopic = new Mock<Topic>();
+
+            // Act
+            var topics = topicService.GetAllTopics();
+
+            // Assert
+            Assert.IsInstanceOf<IQueryable<Topic>>(topics);
+        }
+    }
+}

11c4a4f1529a889639a34df09034472b56348fe0
YavorYanchev <yavor_yanchev_bs@abv.bg>
2017-03-16 12:41:59 +0200

    Changed theme

--- Langcademy/Web/Langcademy.Web/App_Start/BundleConfig.cs
+++ Langcademy/Web/Langcademy.Web/App_Start/BundleConfig.cs
@@ -20,7 +20,8 @@
 
         private static void RegisterStyles(BundleCollection bundles)
         {
-            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/bootstrap.css", "~/Content/site.css"));
+            //bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/bootstrap.css", "~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/bootstrap-flatly.css", "~/Content/site.css"));
         }
     }
 }

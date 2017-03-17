5f4a9def1b1a48766582c46856b18a0688feb994
YavorYanchev <yavor_yanchev_bs@abv.bg>
2017-03-17 22:52:12 +0200

    Updated viewModel for registration

--- Langcademy/Langcademy.Common/GlobalConstants.cs
+++ Langcademy/Langcademy.Common/GlobalConstants.cs
@@ -4,5 +4,16 @@
     {
         public const string AdministratorRoleName = "Administrator";
         public const int PasswordMinLength = 1;
+        public const int UserNameMinLength = 4;
+        public const int UserNameMaxLength = 30;
+
+        public const int FirstNameMinLength = 2;
+        public const int FirstNameMaxLength = 30;
+
+        public const int LastNameMinLength = 2;
+        public const int LastNameMaxLength = 30;
+
+        public const int AvatarImageUrlMaxLength = 1024;
+        public const int EmailMaxLength = 100;
     }
 }
--- Langcademy/Web/Langcademy.Web/ViewModels/Account/RegisterViewModel.cs
+++ Langcademy/Web/Langcademy.Web/ViewModels/Account/RegisterViewModel.cs
@@ -1,7 +1,8 @@
-﻿namespace Langcademy.Web.ViewModels.Account
-{
-    using System.ComponentModel.DataAnnotations;
+﻿using Langcademy.Common;
+using System.ComponentModel.DataAnnotations;
 
+namespace Langcademy.Web.ViewModels.Account
+{
     public class RegisterViewModel
     {
         [Required]
@@ -10,7 +11,7 @@
         public string Email { get; set; }
 
         [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = Common.GlobalConstants.PasswordMinLength)]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = GlobalConstants.PasswordMinLength)]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
         public string Password { get; set; }
@@ -19,5 +20,25 @@
         [Display(Name = "Confirm password")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
+
+        [Required]
+        [MinLength(GlobalConstants.UserNameMinLength)]
+        [MaxLength(GlobalConstants.UserNameMaxLength)]
+        [Display(Name = "Username")]
+        public string UserName { get; set; }
+
+        [Required]
+        [MinLength(GlobalConstants.FirstNameMinLength)]
+        [MaxLength(GlobalConstants.FirstNameMaxLength)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MinLength(GlobalConstants.LastNameMinLength)]
+        [MaxLength(GlobalConstants.LastNameMaxLength)]
+        public string LastName { get; set; }
+
+        [Display(Name = "Image url")]
+        [MaxLength(GlobalConstants.AvatarImageUrlMaxLength)]
+        public string ImageUrl { get; set; }
     }
 }

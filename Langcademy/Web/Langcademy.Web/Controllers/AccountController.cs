4b4383cc69f7282e44f22853fe8082e343c1edf9
YavorYanchev <yavor_yanchev_bs@abv.bg>
2017-03-17 23:01:28 +0200

    Updated register view

--- Langcademy/Web/Langcademy.Web/Controllers/AccountController.cs
+++ Langcademy/Web/Langcademy.Web/Controllers/AccountController.cs
@@ -170,7 +170,11 @@
         {
             if (this.ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var user = new ApplicationUser
+                {
+                    UserName = model.Email,
+                    Email = model.Email
+                };
                 var result = await this.UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {

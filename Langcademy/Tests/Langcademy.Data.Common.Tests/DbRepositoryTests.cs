0be1815304b5f6cd0e7425fd13bc1961dab99e2b
YavorYanchev <yavor_yanchev_bs@abv.bg>
2017-03-19 18:33:51 +0200

    Added test to test the build

--- Langcademy/Tests/Langcademy.Data.Common.Tests/DbRepositoryTests.cs
+++ Langcademy/Tests/Langcademy.Data.Common.Tests/DbRepositoryTests.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Langcademy.Data.Common;
+using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,14 @@ using System.Threading.Tasks;
 
 namespace Langcademy.Data.Common.Tests
 {
-    class DbRepositoryTests
+    [TestFixture]
+    public class DbRepositoryTests
     {
+        [Test]
+        public void NotInterestingTest()
+        {
+            
+            Assert.IsTrue(true);
+        }
     }
 }

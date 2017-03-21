1a8e2944a451564647a1f36b6123d31d52bc4b30
YavorYanchev <yavor_yanchev_bs@abv.bg>
2017-03-21 16:09:09 +0200

    Added admin topics management but delete is not implemented yet

--- Langcademy/Web/Langcademy.Web/Areas/Admin/Controllers/TopicsAdministrationController.cs
+++ Langcademy/Web/Langcademy.Web/Areas/Admin/Controllers/TopicsAdministrationController.cs
@@ -1,4 +1,5 @@
 ﻿using Langcademy.Common;
+using Langcademy.Services.Data.Contracts;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,15 +8,23 @@ using System.Web.Mvc;
 
 namespace Langcademy.Web.Areas.Admin.Controllers
 {
-   
+
     [Authorize(Roles = GlobalConstants.AdministratorRoleName)]
     //[Authorize]
     public class TopicsAdministrationController : Controller
     {
+        private readonly ITopicsService topics;
+
+        public TopicsAdministrationController(ITopicsService topics)
+        {
+            this.topics = topics;
+        }
+
         // GET: Admin/TopicsAdministration
         public ActionResult Index()
         {
-            return View();
+            var allTopics = this.topics.GetAllTopics();
+            return View(allTopics);
         }
     }
 }

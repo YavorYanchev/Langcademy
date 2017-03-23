58b351e39c6bc8483d1305fa8595f8ed28b22194
YavorYanchev <yavor_yanchev_bs@abv.bg>
2017-03-23 16:52:24 +0200

    Added administration for the submissions

---
+++ Langcademy/Web/Langcademy.Web/Areas/Admin/Controllers/SubmissionsAdministrationController.cs
@@ -0,0 +1,28 @@
+﻿using Langcademy.Common;
+using Langcademy.Services.Data.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Langcademy.Web.Areas.Admin.Controllers
+{
+    [Authorize(Roles = GlobalConstants.AdministratorRoleName)]
+    public class SubmissionsAdministrationController:Controller
+    {
+
+        private readonly ITopicSubmissionsService submissions;
+
+        public SubmissionsAdministrationController(ITopicSubmissionsService submissions)
+        {
+            this.submissions = submissions;
+        }
+
+        public ActionResult Index()
+        {
+            var allSubmissions = this.submissions.GetAllTopicSubmissions();
+            return View(allSubmissions);
+        }
+    }
+}

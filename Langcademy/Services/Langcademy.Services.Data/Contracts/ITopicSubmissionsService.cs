2dd22a799ff70aa7c334edc5877cfaf5b2f1473f
YavorYanchev <yavor_yanchev_bs@abv.bg>
2017-03-23 14:56:05 +0200

    Added interface for the topicsSubmissions

---
+++ Langcademy/Services/Langcademy.Services.Data/Contracts/ITopicSubmissionsService.cs
@@ -0,0 +1,20 @@
+﻿using Langcademy.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Langcademy.Services.Data.Contracts
+{
+    public interface ITopicSubmissionsService
+    {
+        void Add(TopicSubmission topicSubmission);
+
+        TopicSubmission GetById(int id);
+
+        IQueryable<TopicSubmission> GetAllTopicSubmissions();
+
+
+    }
+}
---
+++ Langcademy/Services/Langcademy.Services.Data/TopicSubmissionsService.cs
@@ -0,0 +1,48 @@
+﻿using Langcademy.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Langcademy.Services.Data
+{
+    //public class TopicSubmissionsService:ITopicSubmissionsService
+    //{
+    //    private readonly IIdentifierProvider identifierProvider;
+    //    private readonly IDbRepository<Topic> topics;
+
+    //    public TopicsService(IDbRepository<Topic> topics, IIdentifierProvider identifierProvider)
+    //    {
+    //        this.topics = topics;
+    //        this.identifierProvider = identifierProvider;
+    //    }
+
+    //    public void Add(TopicSubmission topic)
+    //    {
+    //        if (topic == null)
+    //        {
+    //            throw new ArgumentNullException("Topic should not be null");
+    //        }
+
+    //        this.topics.Add(topic);
+    //        this.topics.Save();
+    //    }
+
+    //    public Topic GetById(int id)
+    //    {
+    //        var topic = this.topics.GetById(id);
+    //        if (topic == null)
+    //        {
+    //            throw new ArgumentNullException("Topic with the provided id is not found");
+    //        }
+
+    //        return topic;
+    //    }
+
+    //    public IQueryable<Topic> GetAllTopics()
+    //    {
+    //        return this.topics.All();
+    //    }
+    //}
+}

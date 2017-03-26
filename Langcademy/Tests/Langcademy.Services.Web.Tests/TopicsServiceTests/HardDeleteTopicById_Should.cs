b7d8f9f3852067f2ffcd4f44951b314611d3d6e9
YavorYanchev <yavor_yanchev_bs@abv.bg>
2017-03-26 21:07:57 +0300

    Adding more tests for the topicService

---
+++ Langcademy/Tests/Langcademy.Services.Web.Tests/TopicsServiceTests/HardDeleteTopicById_Should.cs
@@ -0,0 +1,54 @@
+﻿using Langcademy.Data.Common;
+using Langcademy.Data.Models;
+using Langcademy.Services.Data;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Langcademy.Services.Web.Tests.TopicsServiceTests
+{
+    [TestFixture]
+    public class HardDeleteTopicById_Should
+    {
+        [TestCase(int.MinValue)]
+        [TestCase(int.MaxValue)]
+
+        public void CallHardDeleteTopicByIdOnce(int id)
+        {
+            // Arrange
+            var mockedRepository = new Mock<IDbRepository<Topic>>();
+            var mockedIdentifier = new Mock<IIdentifierProvider>();
+            var topicService = new TopicsService(mockedRepository.Object, mockedIdentifier.Object);
+            var mockedTopic = new Mock<Topic>();
+
+            // Act
+            topicService.HardDeleteTopicById(id);
+
+            // Assert
+            mockedRepository.Verify(m => m.HardDeleteById(id), Times.Once);
+
+        }
+
+        [TestCase(int.MaxValue)]
+
+        public void CallSaveOnTheRepository(int id)
+        {
+            // Arrange
+            var mockedRepository = new Mock<IDbRepository<Topic>>();
+            var mockedIdentifier = new Mock<IIdentifierProvider>();
+            var topicService = new TopicsService(mockedRepository.Object, mockedIdentifier.Object);
+            var mockedTopic = new Mock<Topic>();
+
+            // Act
+            topicService.HardDeleteTopicById(id);
+
+            // Assert
+            mockedRepository.Verify(m => m.Save(), Times.Once);
+
+        }
+    }
+    }

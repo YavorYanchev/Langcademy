0ab7117e165bb2fde8c5b38d6514d57603a6163a
YavorYanchev <yavor_yanchev_bs@abv.bg>
2017-03-26 16:28:23 +0300

    Added more tests for the topicController

---
+++ Langcademy/Tests/Langcademy.Web.Controllers.Tests/TopicsControllerTests/AllTopics_Should.cs
@@ -0,0 +1,55 @@
+﻿using Langcademy.Services.Data.Contracts;
+using Langcademy.Web.ViewModels.Topics;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestStack.FluentMVCTesting;
+
+namespace Langcademy.Web.Controllers.Tests.TopicsControllerTests
+{
+    [TestFixture]
+    public class AllTopics_Should
+    {
+        [Test]
+        public void RenderAllTopicsPartialView()
+        {
+            // Arrange
+            var topicServiceMock = new Mock<ITopicsService>();
+            var usersServiceMock = new Mock<IUsersService>();
+            var submissionsServiceMock = new Mock<ITopicSubmissionsService>();
+            var topicController = new TopicsController(
+                topicServiceMock.Object,
+                usersServiceMock.Object,
+                submissionsServiceMock.Object);
+
+            // Act & Assert
+            topicController
+                .WithCallTo(t => t.AllTopics())
+                .ShouldRenderPartialView("_AllTopicsPartial");
+        }
+
+        [Test]
+        public void UseTopicViewModelWithTheCorrectPartialView()
+        {
+            // Arrange
+            var topicServiceMock = new Mock<ITopicsService>();
+            var usersServiceMock = new Mock<IUsersService>();
+            var submissionsServiceMock = new Mock<ITopicSubmissionsService>();
+            var topicController = new TopicsController(
+                topicServiceMock.Object,
+                usersServiceMock.Object,
+                submissionsServiceMock.Object);
+
+            // Act & Assert
+            topicController
+                .WithCallTo(t => t.AllTopics())
+                .ShouldRenderPartialView("_AllTopicsPartial")
+                .WithModel<IEnumerable<TopicViewModel>>();
+        }
+
+    }
+}
---
+++ Langcademy/Tests/Langcademy.Web.Controllers.Tests/TopicsControllerTests/Create_Should.cs
@@ -0,0 +1,45 @@
+﻿using Langcademy.Services.Data.Contracts;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Reflection;
+using System.Web.Mvc;
+using TestStack.FluentMVCTesting;
+
+namespace Langcademy.Web.Controllers.Tests.TopicsControllerTests
+{
+    [TestFixture]
+    public class Create_Should
+    {
+        [Test]
+        public void ReturnDefaultView()
+        {
+            // Arrange
+            var topicServiceMock = new Mock<ITopicsService>();
+            var usersServiceMock = new Mock<IUsersService>();
+            var submissionsServiceMock = new Mock<ITopicSubmissionsService>();
+            var topicController = new TopicsController(
+                topicServiceMock.Object,
+                usersServiceMock.Object,
+                submissionsServiceMock.Object);
+
+            // Act & Assert
+            topicController
+                .WithCallTo(t => t.Create())
+                .ShouldRenderDefaultView();
+        }
+
+        [Test]
+        public void HasAuthorizeAttribute()
+        {
+            // Arrange & Act
+            var createMethod = typeof(TopicsController).GetMethod("Create", new Type[0]);
+
+            // using reflection
+            var attribute = createMethod.GetCustomAttribute(typeof(AuthorizeAttribute));
+
+            // Assert
+            Assert.IsNotNull(attribute);
+        }
+    }
+}

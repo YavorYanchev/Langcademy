f2433ce63eb575683b7bdd51bf254fc94da1a35b
YavorYanchev <yavor_yanchev_bs@abv.bg>
2017-03-27 00:56:34 +0300

    Added tests for the HomeController

---
+++ Langcademy/Tests/Langcademy.Web.Controllers.Tests/HomeControllerTests/Index_Should.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using TestStack.FluentMVCTesting;
+
+namespace Langcademy.Web.Controllers.Tests.HomeControllerTests
+{
+    [TestFixture]
+    public class Index_Should
+    {
+        [Test]
+        public void RenderDefaultView()
+        {
+            // Arrange
+            var homeController = new HomeController();
+
+            // Act & Assert
+            homeController.WithCallTo(h => h.Index()).ShouldRenderDefaultView();
+
+        }
+    }
+}

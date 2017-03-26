c967dbd4fe65d72d42fbdcebe447f9a32dd299e3
YavorYanchev <yavor_yanchev_bs@abv.bg>
2017-03-27 00:19:21 +0300

    Added more tests for topicSubmissionService

---
+++ Langcademy/Tests/Langcademy.Services.Web.Tests/TopicSubmissionsServiceTests/GetAllTopicSubmissions_Should.cs
@@ -0,0 +1,32 @@
+﻿using Langcademy.Data.Common;
+using Langcademy.Data.Models;
+using Langcademy.Services.Data;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Langcademy.Services.Web.Tests.TopicSubmissionsServiceTests
+{
+    [TestFixture]
+    public class GetAllTopicSubmissions_Should
+    {
+        [Test]
+        public void ReturnIQuerableOfSubmission()
+        {
+            // Arrange
+            var mockedRepository = new Mock<IDbRepository<TopicSubmission>>();
+            var mockedIdentifier = new Mock<IIdentifierProvider>();
+            var submissionService = new TopicSubmissionsService(mockedRepository.Object, mockedIdentifier.Object);
+
+            // Act
+            var submissions = submissionService.GetAllTopicSubmissions();
+
+            // Assert
+            Assert.IsInstanceOf<IQueryable<TopicSubmission>>(submissions);
+        }
+    }
+}
---
+++ Langcademy/Tests/Langcademy.Services.Web.Tests/TopicSubmissionsServiceTests/GetById_Should.cs
@@ -0,0 +1,46 @@
+﻿using Langcademy.Data.Common;
+using Langcademy.Data.Models;
+using Langcademy.Services.Data;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Langcademy.Services.Web.Tests.TopicSubmissionsServiceTests
+{
+    [TestFixture]
+    public class GetById_Should
+    {
+        [TestCase(int.MinValue)]
+        [TestCase(int.MaxValue)]
+        public void ThrowArgumentNullExceptionWhenSubmissionIsNotFound(int invalidId)
+        {
+            // Arrange
+            var mockedRepository = new Mock<IDbRepository<TopicSubmission>>();
+            var mockedIdentifier = new Mock<IIdentifierProvider>();
+            var submissionService = new TopicSubmissionsService(mockedRepository.Object, mockedIdentifier.Object);
+
+
+            // Act & Assert
+            Assert.Throws<ArgumentNullException>(() => submissionService.GetById(invalidId));
+        }
+
+        [TestCase(int.MinValue)]
+        [TestCase(int.MaxValue)]
+
+        public void ThrowArgumentNullExceptionWhenSubmissionIsNotFoundWithCorrectMessage(int invalidId)
+        {
+            // Arrange
+            var mockedRepository = new Mock<IDbRepository<TopicSubmission>>();
+            var mockedIdentifier = new Mock<IIdentifierProvider>();
+            var submissionService = new TopicSubmissionsService(mockedRepository.Object, mockedIdentifier.Object);
+
+            // Act & Assert
+            var exc = Assert.Throws<ArgumentNullException>(() => submissionService.GetById(invalidId));
+            StringAssert.Contains("not found", exc.Message);
+        }
+    }
+}

73874f3d9ced946db7f470de8f5c5a875dcbaccc
YavorYanchev <yavor_yanchev_bs@abv.bg>
2017-03-27 00:03:31 +0300

    more tests for the TopicsService

---
+++ Langcademy/Tests/Langcademy.Services.Web.Tests/TopicsServiceTests/GetTopicByNameOrDescription_Should.cs
@@ -0,0 +1,34 @@
+﻿using Langcademy.Data.Common;
+using Langcademy.Data.Models;
+using Langcademy.Services.Data;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Langcademy.Services.Web.Tests.TopicsServiceTests
+{
+    [TestFixture]
+    public class GetTopicByNameOrDescription_Should
+    {
+        [TestCase("searchTermString")]
+        public void ReturnIEnumerableOfTopic(string searchTerm)
+        {
+            // Arrange
+            var mockedRepository = new Mock<IDbRepository<Topic>>();
+            var mockedIdentifier = new Mock<IIdentifierProvider>();
+            var topicService = new TopicsService(mockedRepository.Object, mockedIdentifier.Object);
+            var mockedTopic = new Mock<Topic>();
+
+            // Act
+            var result = topicService.GetTopicByNameOrDescription(searchTerm);
+
+            // Assert
+            Assert.IsInstanceOf<IEnumerable<Topic>>(result);
+
+        }
+    }
+}

8a85b7da18ed6885abcdae22cd11ead4344937d5
YavorYanchev <yavor_yanchev_bs@abv.bg>
2017-03-26 19:22:39 +0300

    AdministrationTopicController tested

---
+++ Langcademy/Tests/Langcademy.Web.Controllers.Tests/AreaAdminTests/TopicsAdministrationControllerTests/Delete_Should.cs
@@ -0,0 +1,47 @@
+﻿using Langcademy.Services.Data.Contracts;
+using Langcademy.Web.Areas.Admin.Controllers;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestStack.FluentMVCTesting;
+
+namespace Langcademy.Web.Controllers.Tests.AreaAdminTests.TopicsAdministrationControllerTests
+{
+    [TestFixture]
+    public class Delete_Should
+    {
+        [TestCase(1)]
+        [TestCase(64937)]
+        public void ReturnDefaultView(int id)
+        {
+            // Arrange
+            var topicServiceMock = new Mock<ITopicsService>();
+            var topicAdministrationController = new TopicsAdministrationController(
+                topicServiceMock.Object);
+
+            // Act & Assert
+            topicAdministrationController
+                .WithCallTo(t => t.Delete(id))
+                .ShouldRedirectTo(t => t.Index());
+        }
+
+        [TestCase(64937)]
+        public void CallHardDeleteTopicByIdOnce(int id)
+        {
+            // Arrange
+            var topicServiceMock = new Mock<ITopicsService>();
+            var topicAdministrationController = new TopicsAdministrationController(
+                topicServiceMock.Object);
+
+            // Act
+            topicAdministrationController.Delete(id);
+
+            // Assert
+            topicServiceMock.Verify(t => t.HardDeleteTopicById(id), Times.Once);
+        }
+    }
+}

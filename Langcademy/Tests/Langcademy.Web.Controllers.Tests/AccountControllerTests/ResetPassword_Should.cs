ffdebe284e8137fd4dea857c400a04e38b8eed0d
YavorYanchev <yavor_yanchev_bs@abv.bg>
2017-03-27 00:48:32 +0300

    Added tests for the AccountController

---
+++ Langcademy/Tests/Langcademy.Web.Controllers.Tests/AccountControllerTests/ResetPassword_Should.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestStack.FluentMVCTesting;
+
+namespace Langcademy.Web.Controllers.Tests.AccountControllerTests
+{
+    [TestFixture]
+    public class ResetPassword_Should
+    {
+        [TestCase("notNullString")]
+        public void ReturnDefaultViewWhenCodeIsNotNull(string code)
+        {
+            // Arrange
+            var accController = new AccountController();
+
+            // Act & Assert
+            accController.WithCallTo(a => a.ResetPassword(code)).ShouldRenderDefaultView();
+
+        }
+
+        [TestCase(null)]
+        public void ReturnDefaultErrorViewWhenCodeIsNull(string code)
+        {
+            // Arrange
+            var accController = new AccountController();
+
+            // Act & Assert
+            accController.WithCallTo(a => a.ResetPassword(code)).ShouldRenderView("Error");
+
+        }
+    }
+}
